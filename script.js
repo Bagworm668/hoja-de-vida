4078885e0f384338bbf21feb1f2c68971a708f4a
Bagworm668 <83565249+Bagworm668@users.noreply.github.com>
2024-02-24 17:11:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let elemento;
+elemento = document;
+elemento1 = document.head;
+elemento2 = document.body;
+elemento3 = document.characterSet;
+elemento4 = document.contentType;
+
+
+
+console.log(elemento);
+console.log(elemento1);
+console.log(elemento2);
+console.log(elemento3);
+console.log(elemento4);
